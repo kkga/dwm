591c75dc64e2eacb12bb7d274f1c2bc31eb049fa
Gadzhi Kharkharov <me@kkga.me>
2020-08-26 14:10:18 +0300

    update bindings

--- config.def.h
+++ config.def.h
@@ -11,15 +11,14 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Iosevka Fixed SS10 Extended:size=10" };
 static const char dmenufont[]       = "Iosevka Fixed SS10 Extended:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#1C1C1C";
+static const char col_gray2[]       = "#333333";
+static const char col_gray3[]       = "#666666";
+static const char col_gray4[]       = "#EEEEEE";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_gray3, col_gray4  },
 };
 
 /* tagging */
@@ -60,19 +59,19 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "kitty", NULL };
-static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "1", "+5%",     NULL };
-static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "1", "-5%",     NULL };
-static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "1", "toggle",  NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray2, "-nf", col_gray4, "-sb", col_gray3, "-sf", col_gray4, NULL };
+static const char *termcmd[]    = { "kitty", NULL };
+static const char *lockcmd[]    = { "slock", NULL };
+static const char *volupcmd[]   = { "volume.sh", "-i", "5",     NULL };
+static const char *voldowncmd[] = { "volume.sh", "-d", "5",     NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY, 		        XK_F11,    spawn,	   {.v = downvol } },
-	{ MODKEY,                       XK_F9,     spawn,	   {.v = mutevol } },
-	{ MODKEY,                       XK_F12,    spawn,	   {.v = upvol   } },
+	{ MODKEY,						XK_minus,  spawn,	       {.v = voldowncmd } },
+	{ MODKEY,                       XK_equal,  spawn,	       {.v = volupcmd   } },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ControlMask,           XK_q,      spawn,          {.v = lockcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -82,10 +81,10 @@ static Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,			            XK_q,      killclient,     {0} },
+	{ MODKEY,                       XK_z,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_x,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_c,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
